e7eba0cf15d50868abc06d452ad295466f6016c4
Roman Hromov <rv.hromov@gmail.com>
2017-02-06 19:37:04 +0200

    Building a half of pyramid for Mario

---
+++ mario_less_comfortable.c
@@ -0,0 +1,34 @@
+﻿#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+   
+    // Запрашиваем высоту, пока нe выполнится условие height є [0; 23]
+    do 
+    {
+        printf("Height: ");
+        height = get_int(); 
+    }
+    while(height < 0 || height > 23);
+    
+    // Количество строк (высота пирамиды)
+    for(int i = 1; i <= height; i++)
+    {
+        // Пробелы
+        for(int j = height - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        
+        // Решетки
+        for(int k = 0; k < (1 + i); k++)
+        {
+            printf("#");
+        }
+        
+        // Переход на новую строку
+        printf("\n");
+    }
+}
