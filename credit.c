2b1aad55e26b4975d2ca898c6446eb79c473859b
rvhromov <rv.hromov@gmail.com>
2017-02-08 15:00:46 +0200

    Defining card by number

--- credit.c
+++ credit.c
@@ -1,17 +1,33 @@
-#include <cs50.h>
+﻿#include <cs50.h>
 #include <stdio.h>
+
+long long get_card_number();
+int get_digits_sum(long long cc_number);
+void get_type_card(int total_sum, int digits, long long cc_number);
 
 int main(void)
 {
     long long cc_number;                // Номер кредитки
-    int digit;                          // Текущая цифра номера кредитки
-    int fraction_digit = 0;             // Дробная часть числа
-    int first_to_last_digits_sum = 0;   // Сумма цифр не умножаемых на 2
-    int second_to_last_digits_sum = 0;  // Сумма цифр умножаемых на 2
-    int multi_by_two = 0;               // Текущая цифра умноженая на 2
-    int total_sum = 0;                  // Общая сумма цифр кредитки после всех действий
     int amount_of_digits = 2;           // Количество цифр в номере кредитки
-    long long card_type;                // Тип кредитки
+    int total_sum = 0;                  // Сумма цифр кредитки по алгоритму Луна
+    
+    cc_number = get_card_number();
+    total_sum = get_digits_sum(cc_number);
+    
+    // Получаем 2 первых цифры и количество цифр кредитки
+    // для подальшего определения типа кредитки
+    while(cc_number > 100)
+    {
+        cc_number /= 10;
+        amount_of_digits++;
+    }
+    
+    get_type_card(total_sum, amount_of_digits, cc_number);
+}
+
+long long get_card_number()
+{
+    long long cc_number;    // Номер кредитки
     
     do
     {
@@ -20,7 +36,16 @@ int main(void)
     }
     while(cc_number < 0);
     
-    card_type = cc_number;
+    return cc_number;
+}
+
+int get_digits_sum(long long cc_number)
+{
+    int digit;                          // Текущая цифра номера кредитки
+    int fraction_digit = 0;             // Дробная часть числа
+    int first_to_last_digits_sum = 0;   // Сумма цифр не умножаемых на 2
+    int second_to_last_digits_sum = 0;  // Сумма цифр умножаемых на 2
+    int multi_by_two = 0;               // Текущая цифра умноженая на 2
     
     while(cc_number > 0)
     {
@@ -52,15 +77,11 @@ int main(void)
         cc_number /= 10;
     }
     
-    total_sum = first_to_last_digits_sum + second_to_last_digits_sum;
-    
-    // Получаем 2 первых цифры и количество цифр кредитки
-    while(card_type > 100)
-    {
-        card_type /= 10;
-        amount_of_digits++;
-    }
-    
+    return first_to_last_digits_sum + second_to_last_digits_sum;
+}
+
+void get_type_card(int total_sum, int digits, long long cc_number)
+{
     // Проверяем является ли карточка действительной
     if(total_sum % 10 != 0)
     {
@@ -68,19 +89,19 @@ int main(void)
     }
     
     // Проверяем, что это карта AmericanExpress
-    if(amount_of_digits == 15 && (card_type == 34 || card_type == 37))
+    if(digits == 15 && (cc_number == 34 || cc_number== 37))
     {
         printf("AMEX\n");
     }
     
     // Проверяем, что это карта MasterCard
-    if(amount_of_digits == 16 && (card_type == 51 || card_type == 52 || card_type == 53 || card_type == 54 || card_type == 55))
+    if(digits == 16 && (cc_number == 51 || cc_number == 52 || cc_number == 53 || cc_number == 54 || cc_number == 55))
     {
         printf("MASTERCARD\n");    
     }
     
     // Проверяем, что это карта Visa
-    if((amount_of_digits == 13 || amount_of_digits == 16) && card_type / 10 == 4)
+    if((digits == 13 || digits == 16) && cc_number / 10 == 4)
     {
         printf("VISA\n");
     }
