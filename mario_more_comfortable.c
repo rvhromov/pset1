dddbf4747dee68ba442a8f9bb12b3c66259a1e21
rvhromov <rv.hromov@gmail.com>
2017-02-08 15:03:04 +0200

    Building a whole pyramid for Mario

--- mario_more_comfortable.c
+++ mario_more_comfortable.c
@@ -1,7 +1,27 @@
-#include <cs50.h>
+﻿#include <cs50.h>
 #include <stdio.h>
 
+int get_height();
+void draw_spaces(int height, int i);
+void draw_hashes(int i);
+
 int main(void)
+{
+    // Высота пирамиды
+    int height = get_height();
+    
+    // Рисуем пирамиду
+    for(int i = 1; i <= height; i++)
+    {
+        draw_spaces(height, i);
+        draw_hashes(i);
+        printf("  ");
+        draw_hashes(i);
+        printf("\n");
+    }
+}
+
+int get_height()
 {
     int height;
    
@@ -13,31 +33,23 @@ int main(void)
     }
     while(height < 0 || height > 23);
     
-    // Количество строк (высота пирамиды)
-    for(int i = 1; i <= height; i++)
+    return height;
+}
+
+void draw_spaces(int height, int i)
+{
+    // Рисуем пробелы
+    for(int j = height - i; j > 0; j--)
     {
-        // Пробелы для левой части пирамиды
-        for(int j = height - i; j > 0; j--)
-        {
-            printf(" ");
-        }
-        
-        // Решетки для левой части пирамиды
-        for(int k = 0; k < i; k++)
-        {
-            printf("#");
-        }
-        
-        // Пробелы между частями пирамиды
-        printf("  ");
-        
-        // Решетки для правой части пирамиды
-        for(int l = 0; l < i; l++)
-        {
-            printf("#");
-        }
-        
-        // Переход на новую строку
-        printf("\n");
+        printf(" ");
+    }
+}
+
+void draw_hashes(int i)
+{
+    // Рисуем решетки
+    for(int k = 0; k < i; k++)
+    {
+        printf("#");
     }
 }
